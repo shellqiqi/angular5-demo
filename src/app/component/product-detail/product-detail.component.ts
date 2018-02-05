f7f1c53c44c2d676eb0bdb946daa2d253fdeaaab
shell7 <shellqiqi@outlook.com>
2018-02-05 19:47:03 +0800

    comment complete

--- src/app/component/product-detail/product-detail.component.ts
+++ src/app/component/product-detail/product-detail.component.ts
@@ -13,6 +13,9 @@ export class ProductDetailComponent implements OnInit {
 
   product: Product;
   comments: Comment[];
+  newRating = 5;
+  newComment: string;
+  isCommentHidden = true;
 
   constructor(
     private route: ActivatedRoute,
@@ -23,5 +26,14 @@ export class ProductDetailComponent implements OnInit {
     const productId = +this.route.snapshot.paramMap.get('productId');
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentForProductId(productId);
+  }
+
+  addComment($event) {
+    const comment = new Comment(0, this.product.id, new Date().toString(), 'Someone', this.newRating, this.newComment);
+    this.comments.unshift(comment);
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
+    this.product.rating = this.comments.reduce((sum, element) => sum + element.rating, 0) / this.comments.length;
   }
 }
--- src/app/component/stars/stars.component.ts
+++ src/app/component/stars/stars.component.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, EventEmitter, Output, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
 
   @Input()
-  private rating = 0;
-  private stars: boolean[];
+  rating = 0;
+  @Output()
+  ratingChange: EventEmitter<number> = new EventEmitter<number>();
+  stars: boolean[] = [];
+  @Input()
+  readonly = true;
 
-  constructor() { }
+  constructor() {
+    for (let i = 0; i < 5; i++) {
+      this.stars.push(false);
+    }
+  }
 
   ngOnInit() {
-    this.stars = [];
-    for (let i = 0; i < 5; i++) {
-      this.stars.push(i > this.rating);
+    this.freshStars();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.freshStars();
+  }
+
+  clickStar($event, i) {
+    if (!this.readonly) {
+      this.rating = i + 1;
+      this.ratingChange.emit(this.rating);
     }
+  }
+
+  private freshStars() {
+      for (let i = 0; i < 5; i++) {
+        this.stars[i] = i > this.rating - 1;
+      }
   }
 }
